8cd834f9a90ba5af90f327fe81bb34d836b80dd5
ruwanga <ruwanga007@gmail.com>
2015-04-19 20:51:27 +0530

    TEST-com.mycompany.app.AppTest.xml

---
+++ diffx/src/main/java/com/mycompany/app/Run.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+import com.mycompany.app.tfs.Main;
+import com.mycompany.app.tfs.Return;
+
+public class Run {
+
+	public int igo() {
+		
+		Main   huge=new Main();
+		Return  illdoit=huge.diffM();
+		int x=illdoit.getChange();
+		return x;
+
+}
+}
